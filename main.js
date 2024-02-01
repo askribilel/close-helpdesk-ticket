7d1b3eb11b5a4f6e28f563b3baff557389bbc1e7
askri bilel <bilel.askri@bee.net.tn>
2024-02-01 14:47:31 +0100

    send cms update

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ const cron = require("node-cron");
 const { autoticketclose } = require("./close-ticket");
 
 
-autoticketclose();
 cron.schedule("03 23 * * *", () => {
-  // autoticketclose();
+  autoticketclose();
 });
 
 
